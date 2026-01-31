4186e94545ed2e8cb54dcfa0d3af68110d711a3c
Keshav Gupta <keshavgupta0924@gmail.com>
2026-01-31 10:04:52 +0530

    Refactor maxProfit method for improved efficiency
    All test cases passed
    Took help of ai to optimise
    Question Link
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

--- Best Time to Buy and Sell Stock.java
+++ Best Time to Buy and Sell Stock.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<i;j++){
-                int a=prices[i]-prices[j];
-                if (a>ans){
-                    ans=a;
+        int minPrice = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; 
+            } else if (price - minPrice > profit) {
+                profit = price - minPrice; 
             }
         }
-        }
-        return ans;
+        return profit;
     }
 }
