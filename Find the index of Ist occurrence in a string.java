5c5d4621ba5ed8446f293bfdc1d0586382a60b8e
Keshav Gupta <keshavgupta0924@gmail.com>
2026-04-02 09:48:15 +0530

    Create Find the index of Ist occurrence in a string.java
    Question Link
    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/

---
+++ Find the index of Ist occurrence in a string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int index=-1;
+        int j=0;
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+                for(j=0;j<needle.length();j++){
+                    if(haystack.charAt(i+j)!=needle.charAt(j)){
+                        break;
+                    }
+                }
+                if(j==needle.length()){
+                    index=i;
+                    break;
+                }
+            }
+        }
+        return index;    
+        }
+}
